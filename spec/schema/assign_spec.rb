9e43e4e3909d46ab4efe8b3f63f48b24a161e330
Doug Youch <dougyouch@gmail.com>
2025-06-21 22:28:30 -0400

    feat: tested assign logic with has one and many associations

--- spec/schema/assign_spec.rb
+++ spec/schema/assign_spec.rb
@@ -9,6 +9,16 @@ describe Schema::Assign do
       attribute :id, :integer
       attribute :name, :string
       attribute :cost, :float, default: 0.0
+
+      has_one(:label) do
+        attribute :id, :integer
+        attribute :name, :string
+      end
+
+      has_many(:tags) do
+        attribute :id, :integer
+        attribute :tag, :string
+      end
     end
     Object.const_set(schema_class_name, kls)
     Object.const_get(schema_class_name)
@@ -20,12 +30,59 @@ describe Schema::Assign do
     {
       id: id,
       name: name,
-      cost: cost
+      cost: cost,
+      label: {
+        id: 4,
+        name: 'test label'
+      },
+      tags: [
+        {
+          id: 2,
+          tag: 'tag2'
+        },
+        {
+          id: 3,
+          tag: 'updated tag3'
+        },
+        {
+          id: nil,
+          tag: 'new tag'
+        }
+      ]
     }
   end
   let(:schema) { schema_class.from_hash(schema_data) }
   let(:model_class) do
-    Struct.new(:id, :name, :cost, :user_id)
+    Struct.new(:id, :name, :cost, :user_id, :label) do
+      def self.label_class
+        @label_class ||= Struct.new(:id, :name)
+      end
+
+      def self.tags_class
+        @tags_class ||=
+          begin
+            kls = Class.new(Array) do
+              def self.tag_class
+                @tag_class ||= Struct.new(:id, :tag)
+              end
+
+              def new(*args)
+                tag = self.class.tag_class.new(*args)
+                append(tag)
+                tag
+              end
+            end
+          end
+      end
+
+      def build_label
+        self.label ||= self.class.label_class.new
+      end
+
+      def tags
+        @tags ||= self.class.tags_class.new
+      end
+    end
   end
   let(:model) { model_class.new }
   let(:include_filter) { nil }
@@ -42,6 +99,8 @@ describe Schema::Assign do
     end
 
     before(:each) do
+      model.tags.new(2, 'tag2')
+      model.tags.new(3, 'tag3')
       subject
     end
 
@@ -49,5 +108,23 @@ describe Schema::Assign do
     it { expect(model.name).to eq(name) }
     it { expect(model.cost).to eq(cost) }
     it { expect(model.user_id).to eq(nil) }
+
+    describe '.update_has_one_association' do
+      it { expect(model.label.id).to eq(4) }
+      it { expect(model.label.name).to eq('test label') }
+    end
+
+    describe '.update_has_many_association' do
+      it { expect(model.tags.size).to eq(3) }
+
+      it { expect(model.tags[0].id).to eq(2) }
+      it { expect(model.tags[0].tag).to eq('tag2') }
+
+      it { expect(model.tags[1].id).to eq(3) }
+      it { expect(model.tags[1].tag).to eq('updated tag3') }
+
+      it { expect(model.tags[2].id).to eq(nil) }
+      it { expect(model.tags[2].tag).to eq('new tag') }
+    end
   end
 end
